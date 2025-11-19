5ff5cc2d083643ff3a0beef79205c58e1d0779e5
Hard1Evil <dmytro.kovinko@nure.ua>
2025-11-19 21:56:07 +0200

    Add files via upload

---
+++ Lab2/task3.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/currency", methods=["GET"])
+def get_currency():
+    
+    today = request.args.get("today")
+    key = request.args.get("key")
+
+    
+    return "USD - 41.5"
+
+if __name__ == "__main__":
+    app.run(port=8000)
