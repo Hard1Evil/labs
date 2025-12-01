86c839304ff2fe9e00e7ef557949d44dd92d8e89
Hard1Evil <dmytro.kovinko@nure.ua>
2025-12-01 16:00:25 +0200

    task3.py

--- Lab2/task3.py
+++ Lab2/task3.py
@@ -2,14 +2,9 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/currency", methods=["GET"])
+@app.get("/currency")
 def get_currency():
-    
-    today = request.args.get("today")
-    key = request.args.get("key")
-
-    
     return "USD - 41.5"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(port=8000)
